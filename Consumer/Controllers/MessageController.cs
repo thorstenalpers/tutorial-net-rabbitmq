d24371182ebb475554a45969b1002e2912b1cf77
netninja <netninja@gmx.de>
2020-11-27 11:52:03 +0100

    Rename and simplify Controller

---
+++ Consumer/Controllers/MessageController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Consumer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+        private readonly ILogger<MessageController> _logger;
+
+        public MessageController(ILogger<MessageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Post([FromBody] string message)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get([FromBody] string message)
+        {
+            return Ok();
+        }
+    }
+}
